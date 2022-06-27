0d236ec7b458d7f2ad19299e370516ccab01c79a
Sebastián Velásquez <sebas99peru@gmail.com>
2022-06-27 13:46:24 -0500

    styles.css y game.js: se añadio las lineas ganadoras cuando hay tres en raya

--- scripts/game.js
+++ scripts/game.js
@@ -6,12 +6,23 @@ let showX_O = true;
 let letter_x = 'x';
 let letter_o = 'o';
 let btnHTML = null;
+let victory = false;
+
+let messageX = 'Ganador player 1';
+let messageO = 'Ganador player 2';
 
 let arrayTable = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
 
 
 //componentes html
-
+let compLine1 = document.getElementById('line-w1');
+let compLine2 = document.getElementById('line-w2');
+let compLine3 = document.getElementById('line-w3');
+let compLine4 = document.getElementById('line-w4');
+let compLine5 = document.getElementById('line-w5');
+let compLine6 = document.getElementById('line-w6');
+let compLine7 = document.getElementById('line-w7');
+let compLine8 = document.getElementById('line-w8');
 
 //funcion principal
 function show(id){
@@ -31,91 +42,160 @@ function show(id){
     }
     
     console.log(arrayTable);
-    browseX_O();
+
+    //0-1-2
+    browseX_O(arrayTable[0], arrayTable[1], arrayTable[2], messageX, messageO, 1);
+
+    //3-4-5
+    browseX_O(arrayTable[3], arrayTable[4], arrayTable[5], messageX, messageO, 2);
+
+    //6-7-8
+    browseX_O(arrayTable[6], arrayTable[7], arrayTable[8], messageX, messageO, 3);
+
+    //0-3-6
+    browseX_O(arrayTable[0], arrayTable[3], arrayTable[6], messageX, messageO, 4);
+
+    //1-4-7
+    browseX_O(arrayTable[1], arrayTable[4], arrayTable[7], messageX, messageO, 5);
+
+    //2-5-8
+    browseX_O(arrayTable[2], arrayTable[5], arrayTable[8], messageX, messageO, 6);
+
+    //0-4-8
+    browseX_O(arrayTable[0], arrayTable[4], arrayTable[8], messageX, messageO, 7);
+    
+    //2-4-6
+    browseX_O(arrayTable[2], arrayTable[4], arrayTable[6], messageX, messageO, 8);
+
+
 }
 
 //otras funciones
-function browseX_O(){
+function browseX_O(figure1, figure2, figure3, texto_X, texto_O, shapeW){
 
-    //0-1-2
-    if(arrayTable[0] == 'x' && arrayTable[1] == 'x' && arrayTable[2] == 'x'){
-        console.log('Ganador jugador 1');
+    if(figure1 == 'x' && figure2 == 'x' && figure3 == 'x'){
+        console.log(texto_X);
+        victory = true;
+        showLinesWinner(shapeW);
         disableButons();
     }
-    else if(arrayTable[0] == 'o' && arrayTable[1] == 'o' && arrayTable[2] == 'o'){
-        console.log('Ganador jugador 2');
-        disableButons();
-    }
-
-    //3-4-5
-    if(arrayTable[3] == 'x' && arrayTable[4] == 'x' && arrayTable[5] == 'x'){
-        console.log('Ganador jugador 1');
-        disableButons();
-    }
-    else if(arrayTable[3] == 'o' && arrayTable[4] == 'o' && arrayTable[5] == 'o'){
-        console.log('Ganador jugador 2');
+    else if(figure1 == 'o' && figure2 == 'o' && figure3 == 'o'){
+        console.log(texto_O);
+        victory = true;
+        showLinesWinner(shapeW);
         disableButons();
     }
 
-    //6-7-8
-    if(arrayTable[6] == 'x' && arrayTable[7] == 'x' && arrayTable[8] == 'x'){
-        console.log('Ganador jugador 1');
-        disableButons();
-    }
-    else if(arrayTable[6] == 'o' && arrayTable[7] == 'o' && arrayTable[8] == 'o'){
-        console.log('Ganador jugador 2');
-        disableButons();
-    }
+}
 
-    //0-3-6
-    if(arrayTable[0] == 'x' && arrayTable[3] == 'x' && arrayTable[6] == 'x'){
-        console.log('Ganador jugador 1');
-        disableButons();
-    }
-    else if(arrayTable[0] == 'o' && arrayTable[3] == 'o' && arrayTable[6] == 'o'){
-        console.log('Ganador jugador 2');
-        disableButons();
-    }
+function showLinesWinner(line){
 
-    //1-4-7
-    if(arrayTable[1] == 'x' && arrayTable[4] == 'x' && arrayTable[7] == 'x'){
-        console.log('Ganador jugador 1');
-        disableButons();
-    }
-    else if(arrayTable[1] == 'o' && arrayTable[4] == 'o' && arrayTable[7] == 'o'){
-        console.log('Ganador jugador 2');
-        disableButons();
-    }
+        switch(line){
+            
+            case 1:
+                console.log('entra en 1');
+                compLine1.classList.add('w1-active');
 
-    //2-5-8
-    if(arrayTable[2] == 'x' && arrayTable[5] == 'x' && arrayTable[8] == 'x'){
-        console.log('Ganador jugador 1');
-        disableButons();
-    }
-    else if(arrayTable[2] == 'o' && arrayTable[5] == 'o' && arrayTable[8] == 'o'){
-        console.log('Ganador jugador 2');
-        disableButons();
-    }
+                compLine2.classList.remove('w2-active');
+                compLine3.classList.remove('w3-active');
+                compLine4.classList.remove('w4-active');
+                compLine5.classList.remove('w5-active');
+                compLine6.classList.remove('w6-active');
+                compLine7.classList.remove('w7-active');
+                compLine8.classList.remove('w8-active');
+            break;
 
-    //0-4-8
-    if(arrayTable[0] == 'x' && arrayTable[4] == 'x' && arrayTable[8] == 'x'){
-        console.log('Ganador jugador 1');
-        disableButons();
-    }
-    else if(arrayTable[0] == 'o' && arrayTable[4] == 'o' && arrayTable[8] == 'o'){
-        console.log('Ganador jugador 2');
-        disableButons();
-    }
+            case 2:
+                console.log('entra en 2');
+                compLine2.classList.add('w2-active');
 
-    //2-4-6
-    if(arrayTable[2] == 'x' && arrayTable[4] == 'x' && arrayTable[6] == 'x'){
-        console.log('Ganador jugador 1');
-        disableButons();
-    }
-    else if(arrayTable[2] == 'o' && arrayTable[4] == 'o' && arrayTable[6] == 'o'){
-        console.log('Ganador jugador 2');
-        disableButons();
-    }
+                compLine1.classList.remove('w1-active');
+                compLine3.classList.remove('w3-active');
+                compLine4.classList.remove('w4-active');
+                compLine5.classList.remove('w5-active');
+                compLine6.classList.remove('w6-active');
+                compLine7.classList.remove('w7-active');
+                compLine8.classList.remove('w8-active');
+            break;
+
+            case 3:
+                console.log('entra en 3');
+                compLine3.classList.add('w3-active');
+
+                compLine1.classList.remove('w1-active');
+                compLine2.classList.remove('w2-active');
+                compLine4.classList.remove('w4-active');
+                compLine5.classList.remove('w5-active');
+                compLine6.classList.remove('w6-active');
+                compLine7.classList.remove('w7-active');
+                compLine8.classList.remove('w8-active');
+            break;
+
+            case 4:
+                console.log('entra en 4');
+                compLine4.classList.add('w4-active');
+
+                compLine1.classList.remove('w1-active');
+                compLine2.classList.remove('w2-active');
+                compLine3.classList.remove('w3-active');
+                compLine5.classList.remove('w5-active');
+                compLine6.classList.remove('w6-active');
+                compLine7.classList.remove('w7-active');
+                compLine8.classList.remove('w8-active');
+            break;
+
+            case 5:
+                console.log('entra en 5');
+                compLine5.classList.add('w5-active');
+
+                compLine1.classList.remove('w1-active');
+                compLine2.classList.remove('w2-active');
+                compLine3.classList.remove('w3-active');
+                compLine4.classList.remove('w4-active');
+                compLine6.classList.remove('w6-active');
+                compLine7.classList.remove('w7-active');
+                compLine8.classList.remove('w8-active');
+            break;
+
+            case 6:
+                console.log('entra en 6');
+                compLine6.classList.add('w6-active');
+
+                compLine1.classList.remove('w1-active');
+                compLine2.classList.remove('w2-active');
+                compLine3.classList.remove('w3-active');
+                compLine4.classList.remove('w4-active');
+                compLine5.classList.remove('w5-active');
+                compLine7.classList.remove('w7-active');
+                compLine8.classList.remove('w8-active');
+            break;
+
+            case 7:
+                console.log('entra en 7');
+                compLine7.classList.add('w7-active');
+
+                compLine1.classList.remove('w1-active');
+                compLine2.classList.remove('w2-active');
+                compLine3.classList.remove('w3-active');
+                compLine4.classList.remove('w4-active');
+                compLine5.classList.remove('w5-active');
+                compLine6.classList.remove('w6-active');
+                compLine8.classList.remove('w8-active');
+            break;
+
+            case 8:
+                console.log('entra en 8');
+                compLine8.classList.add('w8-active');
+
+                compLine1.classList.remove('w1-active');
+                compLine2.classList.remove('w2-active');
+                compLine3.classList.remove('w3-active');
+                compLine4.classList.remove('w4-active');
+                compLine5.classList.remove('w5-active');
+                compLine6.classList.remove('w6-active');
+                compLine7.classList.remove('w7-active');
+            break;
+        }
 }
 
 function disableButons(){
